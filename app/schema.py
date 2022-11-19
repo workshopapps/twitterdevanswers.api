dfb97418ed9ace9a2318b445fb8c1a93b483fab1
Idongesit Inyang <inyangidongesit22@gmail.com>
2022-11-19 09:22:40 +0100

    schemas

--- app/schema.py
+++ app/schema.py
@@ -19,3 +19,51 @@ class ChangePasswordRequest(BaseModel):
 class ForgotPassword(BaseModel):
     pass 
 
+class User(BaseModel):
+    id :  int
+    user_name: str
+    first_name: str
+    last_name : str
+    email: str
+    password_hash : str
+    account_balance: int
+    role: str
+    image_url: str
+
+
+class Question(Base):
+   
+    id : int
+    owner_id: int
+    content : str
+    answered :bool
+    created_at: TIMESTAMP
+    updated_at: TIMESTAMP
+
+class Answer(Base):
+   
+    id:int 
+    owner_id: int
+    question_id:int
+    owner = relationship('User')
+    question = relationship('question')
+
+class Notification(Base):
+
+    id : int
+    owner_id : int
+    content_id: int 
+    owner = relationship('user')
+    content = relationship('answer')
+
+class Tag(Base):
+   
+    tag_id: int 
+    tag_name: str 
+
+class contenTag(Base):
+    
+    question_id : int 
+    tag_id: int 
+    question = relationship('question')
+    tag = relationship('tag')
