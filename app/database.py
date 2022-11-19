3c646b70ba200bc7784c5a884675e2bfb089a8e8
codestronomer <johnrumide6@gmail.com>
2022-11-19 22:51:26 +0100

    [fix] database uri

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ from app.config import settings
 # SQLALCHEMY_DATABASE_URI = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 # production
-SQLALCHEMY_DATABASE_URI = "postgres://rpiefrkjxxncgz:38a33ed0eb25a13b7f4f1d5bd02fee72bad4fc86cbd9d07df6d2bf1e35561890@ec2-44-199-143-43.compute-1.amazonaws.com:5432/d2kq6ba278qupb"
+SQLALCHEMY_DATABASE_URI = "postgresql://rpiefrkjxxncgz:38a33ed0eb25a13b7f4f1d5bd02fee72bad4fc86cbd9d07df6d2bf1e35561890@ec2-44-199-143-43.compute-1.amazonaws.com:5432/d2kq6ba278qupb"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
