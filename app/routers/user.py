cc87e526fd69dc793718c0b14f7789b8c45e15bb
Timmy <84716345+timmySpark@users.noreply.github.com>
2022-11-20 02:23:55 +0000

    chore(BE-6) : fix metadata tags for users

--- app/routers/user.py
+++ app/routers/user.py
@@ -9,12 +9,7 @@ from .. import schema
 
 router = APIRouter(
     prefix='/users',
-    tags=[ 
-        {
-        "name": "Users",
-        "description": "Implementing **CRUD** User operations here "
-        }
-    ]
+    tags=['Users']
 )
 
 
