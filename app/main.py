a87eecfc3265951d8c02011daf6f4e8bbc7683b8
codestronomer <johnrumide6@gmail.com>
2022-11-20 02:36:56 +0100

    [feat] add /like endpoint

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
 
 
-from app.routers import user, notification, questions, auth
+from app.routers import user, notification, questions, auth, like
 from app.database import engine, SessionLocal
 
 
@@ -24,6 +24,7 @@ app.include_router(user.router)
 app.include_router(questions.router)
 app.include_router(notification.router)
 app.include_router(auth.router)
+app.include_router(like.router)
 
 
 @app.get("/")
