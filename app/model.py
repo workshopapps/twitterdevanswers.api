8409485daa5174c69a61fc5f0f023b3a5dde3d4f
timmySpark <timmyspark1@gmail.com>
2022-11-24 18:49:49 +0000

    feat(BE-6): added more fields for the User table and removed redundant codes

--- app/model.py
+++ app/model.py
@@ -8,21 +8,21 @@ from app.database import Base, engine
 
 class User(Base):
     __tablename__ = "user"
-    __table_args__ = {'extend_existing': True}
     user_id = Column(Integer, primary_key=True, nullable=False)
     username = Column(String(15), nullable=False)
     first_name = Column(String(30), nullable=False)
     last_name = Column(String(30), nullable=False)
     email = Column(String(100), nullable=False, unique=True)
+    description = Column(String(400),nullable=True)
     password = Column(String, nullable=False)
-    #account_balance = Column(Integer)
-    #role = Column(String(100))
-    image_url = Column(String(300))
+    image_url = Column(String(300),nullable=True)
+    location = Column(String(100),nullable=True)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
 
 
 class Question(Base):
     __tablename__ = "question"
-    __table_args__ = {'extend_existing': True}
     question_id = Column(Integer, primary_key=True, nullable=False)
     owner_id = Column(Integer, ForeignKey(
         "user.user_id", ondelete="CASCADE"), nullable=False)
@@ -31,12 +31,11 @@ class Question(Base):
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
-    owner = relationship('app.model.User')
+    owner = relationship('User')
 
 
 class Answer(Base):
     __tablename__ = "answer"
-    __table_args__ = {'extend_existing': True}
     answer_id = Column(Integer, primary_key=True, nullable=False)
     owner_id = Column(Integer, ForeignKey(
         "user.user_id", ondelete="CASCADE"), nullable=False)
@@ -45,13 +44,11 @@ class Answer(Base):
     content = Column(String(2000))
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
-    is_answer = Column(Boolean,)
-    owner = relationship('app.model.User')
-    question = relationship('app.model.Question')
+    owner = relationship('User')
+    question = relationship('Question')
 
 
 class Like(Base):
-    __table_args__ = {'extend_existing': True}
     __tablename__ = "likes"
     user_id = Column(Integer, ForeignKey(
         'user.user_id', ondelete="CASCADE"), primary_key=True)
@@ -61,14 +58,13 @@ class Like(Base):
 
 class Notification(Base):
     __tablename__ = "notification"
-    __table_args__ = {'extend_existing': True}
     notification_id = Column(Integer, primary_key=True, nullable=False)
     owner_id = Column(Integer, ForeignKey(
         "user.user_id", ondelete="CASCADE"), nullable=False)
     content_id = Column(Integer, ForeignKey(
         "answer.answer_id", ondelete="CASCADE"), nullable=False)
-    owner = relationship('app.model.User')
-    content = relationship('app.model.Answer')
+    owner = relationship('User')
+    content = relationship('Answer')
     type = Column(String(200), nullable=False)
     unread = Column(Boolean, default=True)
     title = Column(String(200), nullable=False)
@@ -76,21 +72,18 @@ class Notification(Base):
 
 class Tag(Base):
     __tablename__ = 'tag'
-    __table_args__ = {'extend_existing': True}
     tag_id = Column(Integer, primary_key=True, nullable=False)
     tag_name = Column(String(40), nullable=False)
 
 
 class contenTag(Base):
-
     __tablename__ = 'contenTag'
-    __table_args__ = {'extend_existing': True}
     question_id = Column(Integer, ForeignKey(
         "question.question_id", ondelete="CASCADE"),  primary_key=True, nullable=False)
     tag_id = Column(Integer, ForeignKey(
         "tag.tag_id", ondelete="CASCADE"), nullable=False)
-    question = relationship('app.model.Question')
-    tag = relationship('app.model.Tag')
+    question = relationship('Question')
+    tag = relationship('Tag')
 
 
-#Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
