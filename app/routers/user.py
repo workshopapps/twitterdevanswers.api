ba845f6a5d72d265c6fccd8bb125444c1d737517
timmySpark <timmyspark1@gmail.com>
2022-11-25 05:14:10 +0000

    chore(BE-6) : added comments

--- app/routers/user.py
+++ app/routers/user.py
@@ -15,33 +15,42 @@ router = APIRouter(
 )
 
 
-# Get all users
 @router.get('/')
 def fetch_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+   
+    """ List to get all users """
+
     return crud.get_users(db, skip=skip, limit=limit)
 
 
-# Get a User
 @router.get('/{user_id}')
 def fetch_user(user_id: int, db: Session = Depends(get_db)):
+    
+    """ Fetch a user by it's user_id  """
+  
     user = crud.get_user(db, user_id=user_id)
     if not user:
         raise HTTPException(
             status=404, detail=f" user with user_id : {user_id} not found")
-    question = Question.query.filter_by(owner_id = user).order_by(Question.created_at.desc()).all()
     return {"success": True, 'data': user}
 
-# update a user
+
 @router.patch('/edit/{user_id}', response_model=schema.UserUpdate)
 def update_user(user: schema.UserUpdate, user_id: int, db: Session = Depends(get_db)):
+    
+    """ Update a User profile by user_id  """
+    
     user = db.query(User).filter(User.user_id == user_id).first()
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return crud.update_user(db, user=user, user_id=user_id)
 
-# delete a user
+
 @router.delete('/delete/{user_id}')
 def delete_user(user_id: int, db: Session = Depends(get_db)):
+    
+    """ Delete a user by it's user_id  """
+  
     delete_user = crud.delete_user(db, user_id=user_id)
     if not delete_user:
         raise HTTPException(
