babe339018db2a6fb268fce1712f103a56005a9f
codestronomer <johnrumide6@gmail.com>
2022-12-01 19:39:37 +0100

    [fix] add is_admin to column

--- app/model.py
+++ app/model.py
@@ -22,6 +22,7 @@ class User(Base):
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
     account_balance = Column(Integer, default=1000)
+    is_admin = Column(Boolean, nullable=True, default=False)
 
 
 class Following(Base):
@@ -34,7 +35,6 @@ class Following(Base):
     ), nullable=False, primary_key=True)
 
 
-
 class Question(Base):
     __tablename__ = "question"
     __table_args__ = {'extend_existing': True}
@@ -42,7 +42,7 @@ class Question(Base):
     owner_id = Column(Integer, ForeignKey(
         "user.user_id", ondelete="CASCADE"), nullable=False)
     content = Column(String(2000), nullable=False)
-    payment_method = Column(String(100),nullable=False)
+    payment_method = Column(String(100), nullable=False)
     answered = Column(Boolean, server_default='FALSE', nullable=False)
     total_like = Column(Integer, default=0)
     total_unlike = Column(Integer, default=0)
@@ -126,18 +126,19 @@ class Tag(Base):
     questions = relationship("app.model.Question",
                              secondary="question_tags", back_populates="tags")
 
+
 class Blog(Base):
-    
+
     __tablename__ = 'blog'
     __table_args__ = {'extend_existing': True}
-    blog_id = Column(Integer,primary_key=True,nullable=False)
-    title = Column(String,nullable=False)
-    body = Column(String,nullable=False)
+    blog_id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    body = Column(String, nullable=False)
     user = relationship('model.User')
-    date_posted = Column(TIMESTAMP(timezone=True),nullable=False, server_default=text('now()'))
-    blog_user_id = Column(Integer,ForeignKey("user.user_id",ondelete="CASCADE"),nullable=False)    
-
-
+    date_posted = Column(TIMESTAMP(timezone=True),
+                         nullable=False, server_default=text('now()'))
+    blog_user_id = Column(Integer, ForeignKey(
+        "user.user_id", ondelete="CASCADE"), nullable=False)
 
 
 Base.metadata.create_all(bind=engine)
