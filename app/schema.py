3585456d8061329997f4108b3448f5b022bd3287
NikkyXO <alexisdame2017@gmail.com>
2022-12-01 20:00:38 +0100

    initial commit

--- app/schema.py
+++ app/schema.py
@@ -39,11 +39,11 @@ class UserSignInRequest(BaseModel):
     password: str
     confirmPassword: str
 
-    @validator('confirmPassword')
-    def passwords_match(cls, v, values, **kwargs):
-        if 'password' in values and v != values['password']:
-            raise ValueError('passwords do not match')
-        return v
+    # @validator('confirmPassword')
+    # def passwords_match(cls, v, values, **kwargs):
+    #     if 'password' in values and v != values['password']:
+    #         raise ValueError('passwords do not match')
+        # return v
 
 
 class UserUpdate(BaseModel):
