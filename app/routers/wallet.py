1f09afda08435e89ba5a34c505aec56d81a3e3d3
NikkyXO <alexisdame2017@gmail.com>
2022-12-02 02:37:35 +0100

    Created User Wallet On Signup

--- app/routers/wallet.py
+++ app/routers/wallet.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import relationship, Session
 from sqlalchemy import Column, String, Integer
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from database import get_db
-from schema import TransactionRequest, WalletItem
+from schema import TransactionRequest
 from model import Wallet
 import schema
 
@@ -35,40 +35,64 @@ def create_wallet( user_id: int,  db: Session = Depends(get_db)):
 
 @router.put('/earn')
 def add_to_wallet(request: schema.TransactionRequest, db: Session = Depends(get_db)):
-	id = request.get('wallet_address')
+	id = request.wallet_address
 	user_account = db.query(Wallet).filter(Wallet.id == id).first()
 	if not user_account:
 		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
 		detail=f'account with the id {id} not available.')
-	amount = request.get('amount')
+	amount = request.amount
 
-	user_account.wallet_balance += amount
+	user_account.balance += amount
 	user_account.deposits_made += 1
 	db.add(user_account)
 	db.commit()
-	db.close()
-	return {"code": "success", "message": "Deposit was successfully added"}
+	db.refresh(user_account)
+	return {"code": "success", 
+	"message": "Deposit was successfully added", 
+	"balance": user_account.balance}
 
 
-@router.put('/pay')
-def add_to_wallet(request: schema.TransactionRequest, db: Session = Depends(get_db)):
+@router.put('/spend')
+def remove_from_wallet(request: schema.TransactionRequest, db: Session = Depends(get_db)):
 
-	id = request.get('user_id')
+	id = request.user_id
 	user_account = db.query(Wallet).filter(Wallet.user_id == id).first()
 
 	if not user_account:
 		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
 		detail=f'account with the id {id} not available.')
 		
-	amount = request.get('amount')
+	amount = request.amount
 	if user_account.balance >= amount:
 
-		user_account.wallet_balance -= amount
+		user_account.balance -= amount
 		user_account.spendings += 1
 		db.add(user_account)
 		db.commit()
-		db.close()
-		return {"code": "success", "message": "Payment was successful"}
+		db.refresh(user_account)
+		return {"code": "success", 
+		"message": "Deposit was successfully added", 
+		"balance": user_account.balance}
 	else:
 		return {"code": "error", "message": "Wallet Balance Insufficience"}
-		
+
+
+
+# {
+#   "Success": true,
+#   "Message": "user added successfully",
+#   "data": {
+#     "user_id": 7,
+#     "userName": "strong",
+#     "email": "strong@example.com",
+#     "wallet": {
+#       "user_id": 7,
+#       "balance": 1000,
+#       "deposits_made": 0,
+#       "spendings": 0,
+#       "id": "e6bd3c1c-8f06-4b3d-b2db-2be00707e972",
+#       "created_at": "2022-12-02T01:11:47.129076"
+#     }
+#   },
+#   "Token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo3LCJleHAiOjE2Njk5NDQ0MDh9.9_IL2vXmGzLhF_SAwyacS03OOLctNJVZ6vOKvckdgk0"
+# }
--- app/schema.py
+++ app/schema.py
@@ -4,6 +4,23 @@ from datetime import datetime
 from typing import Optional, List, Union
 
 
+
+class TransactionRequest(BaseModel):
+	wallet_address: str
+	amount: int
+	user_id: str
+
+	class Config:
+		schema_extra = {
+			"example": {
+				"wallet_address":"e6bd3cthg-8f06-4b3d-b2db-2be907rgade972",
+				"amount": "50",
+				"user_id": "20"
+			}
+		}
+
+
+
 class User(BaseModel):
     user_id:  int
     username: str
