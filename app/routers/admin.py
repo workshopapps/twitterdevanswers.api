526f8cb779b51c43fc34771fa4fbe0dacc585b2b
Chukwuedo Bernadette <100119269+Bernadettechukwuedo@users.noreply.github.com>
2022-12-02 07:39:46 +0100

    files

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -16,8 +16,8 @@ router = APIRouter(
 @router.get('/')
 def fetch_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     """ List all users """
-
-    return crud.get_users(db, skip=skip, limit=limit)
+    user = crud.get_users(db, skip=skip, limit=limit)
+    return user
 
 
 @router.get('/{user_id}', response_model=schema.User)
@@ -39,14 +39,22 @@ def fetch_user(user_id: int, db: Session = Depends(get_db)):
 #     db.refresh()
 #     return new_user
 
-@router.delete("/{user_id}")
-def delete_user(db: Session = Depends(get_db)):
-    """ delete a user by it's user_id  """
-    data =crud.delete_user(db, user_id=user_id)
-    return data
+@router.delete('/delete/{user_id}')
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    """ Delete a user by it's user_id  """
+
+    delete_user = crud.delete_user(db, user_id=user_id)
+    if not delete_user:
+        raise HTTPException(
+            status=404, detail=f"user with user_id : {user_id} does not exist")
+    return delete_user
+
 
 @router.put("/{user_id}")
-def update_user(user:schema.UserUpdate, db: Session = Depends(get_db)):
+def update_user(user:schema.UserUpdate,user_id: int, db: Session = Depends(get_db)):
     """update a user"""
-    user = crud.update_user(db, user, user_id=user_id)
+    user = crud.update_user(db, user=user, user_id=user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+   
     return user
