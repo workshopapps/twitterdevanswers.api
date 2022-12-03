ecd72b24beb083ed3453c76b44e60554313464f9
codestronomer <johnrumide6@gmail.com>
2022-12-03 13:13:51 +0100

    [fix] change app import

--- app/main.py
+++ app/main.py
@@ -2,9 +2,7 @@ from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
 # from app.routers import googleauth
 
-from routers import user, notification, questions, auth, like, answer, following, tag, blog, wallet, admin
-
-
+from app.routers import user, notification, questions, auth, like, answer, following, tag, blog, wallet, admin
 
 
 #  Description of Dev ASk Api
@@ -67,7 +65,6 @@ app.include_router(blog.router)
 app.include_router(admin.router)
 # app.include_router(googleauth.router)
 app.include_router(wallet.router)
-
 
 
 @app.get("/")
