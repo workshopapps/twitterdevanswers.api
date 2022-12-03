c8b1a473372a2b5341ed4c3d11c8274ee7357dc8
Olanike Ogundiran <96661213+NikkyXO@users.noreply.github.com>
2022-12-03 17:45:09 +0100

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -24,10 +24,21 @@ Base = declarative_base()
 
 
 
+@router.get("/wallet/view/{user_id}/user")
+def view_wallet(user_id, db: Session = Depends(get_db)):
+
+    user_account = db.query(Wallet).filter(Wallet.user_id == user_id).first()
+    if not user_account:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'account with the id {user_id} not available.')
+
+    return user_account
+
+
 @router.put('/wallet/earn')
 def add_to_wallet(request: schema.TransactionRequest, db: Session = Depends(get_db)):
-    id = request.wallet_address
-    user_account = db.query(Wallet).filter(Wallet.id == id).first()
+    id = request.user_id
+    user_account = db.query(Wallet).filter(Wallet.user_id == id).first()
     if not user_account:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'account with the id {id} not available.')
@@ -68,20 +79,4 @@ def remove_from_wallet(request: schema.TransactionRequest, db: Session = Depends
         return {"code": "error", "message": "Wallet Balance Insufficience"}
 
 
-# {
-#   "Success": true,
-#   "Message": "user added successfully",
-#   "data": {
-#     "user_id": 7,
-#     "userName": "strong",
-#     "email": "strong@example.com",
-#     "wallet": {
-#       "user_id": 7,
-#       "balance": 1000,
-#       "deposits_made": 0,
-#       "spendings": 0,
-#       "id": "e6bd3c1c-8f06-4b3d-b2db-2be00707e972",
-#       "created_at": "2022-12-02T01:11:47.129076"
-#     }
-#   },
-# }
+
