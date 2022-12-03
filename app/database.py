075b47e6c05458b3cba3ddabd71c2e6ad5286174
codestronomer <johnrumide6@gmail.com>
2022-12-03 13:12:17 +0100

    [fix]

--- app/database.py
+++ app/database.py
@@ -8,7 +8,7 @@ from app.config import settings
 # SQLALCHEMY_DATABASE_URI = f'postgresql://postgres:123456@localhost:5432/devask'
 
 # production
-SQLALCHEMY_DATABASE_URI = "mysql://devask:HNG#9devask@localhost/devask"
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://devask:HNG#9devask@localhost/devask"
 # SQLALCHEMY_DATABASE_URI = "postgresql://hswuttzzoonnuj:84da1bb239e309794b0e3d88aebbc5d8069e908be6667a86e990d31759cf3d22@ec2-54-160-109-68.compute-1.amazonaws.com:5432/dcv4jf04kkfo1d"
 # SQLALCHEMY_DATABASE_URI = "mysql://devask:HNG#9devask@localhost/devask"
 
