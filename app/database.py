2f43a151679bff76aa50dad0bbc667320eb441a0
John Rumide <johnrumide6@gmail.com>
2022-12-04 22:51:03 +0100

    Update datebase.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import settings
+from app.config import settings
 
 # devlopment
 # SQLALCHEMY_DATABASE_URI = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
