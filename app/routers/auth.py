7f9ecdb0da375fd1b15866d792849d3f44517ae7
John Rumide <johnrumide6@gmail.com>
2022-12-08 12:52:37 +0100

    Update auth.py

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -99,6 +99,8 @@ def user_signnup(user_credentials: schema.UserSignInRequest, db: Session = Depen
 			    email=user_credentials.email,
 			    password=user_credentials.password,
 			    )
+    if user_credentials.is_admin:
+	new_user.is_admin = True
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
