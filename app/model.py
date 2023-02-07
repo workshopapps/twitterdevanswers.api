64730bcb4935578c077ab80b27d671d3d1ef0e65
timmySpark <timmyspark1@gmail.com>
2023-02-07 19:59:42 +0000

    [feat] :- changed all model id datatypes from int to String

--- app/model.py
+++ app/model.py
@@ -7,13 +7,12 @@ from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
 from sqlalchemy import MetaData
 from app.database import Base, engine
-from uuid import uuid4
+from uuid import uuid4 ,UUID
 import uuid as uuid_pkg
 import sqlalchemy
 import datetime
 from sqlalchemy import types
 from sqlalchemy_utils.types.choice import ChoiceType
-
 metadata = MetaData()
 
 
@@ -26,8 +25,8 @@ class Transaction(Base):
     __tablename__ = 'transactions'
     __table_args__ = {'extend_existing': True}
 
-    transaction_id = Column(Integer, primary_key=True, nullable=False)
-    user_id = Column(Integer, ForeignKey(
+    transaction_id = Column(String(50), primary_key=True, nullable=False)
+    user_id = Column(String(50), ForeignKey(
         "user.user_id", ondelete="CASCADE"), nullable=True)
     transacion_type = Column(ChoiceType(TYPES))
     amount = Column(Integer, default=0, nullable=False)
@@ -47,7 +46,7 @@ class Wallet(Base):
     total_spent = Column(Integer, default=0, nullable=False)
     total_earned = Column(Integer, default=0, nullable=False)
     is_devask_wallet = Column(Boolean, default=False)
-    user_id = Column(Integer, ForeignKey(
+    user_id = Column(String(50), ForeignKey(
         "user.user_id", ondelete="CASCADE"), nullable=True)
     created_at = Column(
         DateTime, default=datetime.datetime.utcnow, nullable=False)
@@ -56,7 +55,7 @@ class Wallet(Base):
 class User(Base):
     __tablename__ = "user"
     __table_args__ = {'extend_existing': True}
-    user_id = Column(Integer, primary_key=True, nullable=False)
+    user_id = Column(String(50),primary_key=True)
     username = Column(String(100), nullable=False, unique=True)
     first_name = Column(String(30), nullable=True, default=" ")
     last_name = Column(String(30), nullable=True, default=" ")
@@ -66,6 +65,7 @@ class User(Base):
     description = Column(String(400), nullable=True, default=" ")
     password = Column(String(200), nullable=False)
     phone_number = Column(String(30), nullable=True, default=" ")
+    organization = Column(String(400), nullable=True, default=" ")
     work_experience = Column(String(400), nullable=True, default=" ")
     position = Column(String(400), nullable=True, default=" ")
     stack = Column(String(400), nullable=True, default=" ")
@@ -76,8 +76,7 @@ class User(Base):
     image_url = Column(String(300), nullable=True, default=" ")
     location = Column(String(100), nullable=True, default=" ")
     is_admin = Column(Boolean, default=False)
-    account_balance = Column(Integer, ForeignKey(
-        'walletaccount.balance', ondelete="CASCADE"), default=100)
+    account_balance = Column(Integer,default=100)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
     verification_code = Column(String(300), nullable=True, default=" ")
@@ -88,10 +87,10 @@ class User(Base):
 class Following(Base):
     __tablename__ = "following"
     __table_args__ = {'extend_existing': True}
-    user_from = Column(Integer, ForeignKey(
+    user_from = Column(String(50), ForeignKey(
         "user.user_id", ondelete="CASCADE"
     ), nullable=True, primary_key=True)
-    target_user = Column(Integer, ForeignKey(
+    target_user = Column(String(50), ForeignKey(
         "user.user_id", ondelete="CASCADE"
     ), nullable=True, primary_key=True)
 
@@ -99,8 +98,8 @@ class Following(Base):
 class Question(Base):
     __tablename__ = "question"
     __table_args__ = {'extend_existing': True}
-    question_id = Column(Integer, primary_key=True, nullable=False)
-    owner_id = Column(Integer, ForeignKey(
+    question_id = Column(String(50), primary_key=True, nullable=False)
+    owner_id = Column(String(50), ForeignKey(
         "user.user_id", ondelete="CASCADE"), nullable=False)
     title = Column(String(400), nullable=False)
     content = Column(String(2000), nullable=False)
@@ -123,10 +122,10 @@ class Question(Base):
 class Answer(Base):
     __tablename__ = "answer"
     __table_args__ = {'extend_existing': True}
-    answer_id = Column(Integer, primary_key=True, nullable=False)
-    owner_id = Column(Integer, ForeignKey(
+    answer_id = Column(String(50), primary_key=True, nullable=False)
+    owner_id = Column(String(50), ForeignKey(
         "user.user_id", ondelete="CASCADE"), nullable=False)
-    question_id = Column(Integer, ForeignKey(
+    question_id = Column(String(50), ForeignKey(
         "question.question_id", ondelete="CASCADE"), nullable=False)
     content = Column(String(2000))
     created_at = Column(TIMESTAMP(timezone=True),
@@ -141,9 +140,9 @@ class AnswerVote(Base):
     __tablename__ = "answer_vote"
     __table_args__ = {'extend_existing': True}
     id = Column(Integer, primary_key=True)
-    owner_id = Column(Integer, ForeignKey(
+    owner_id = Column(String(50), ForeignKey(
         "user.user_id", ondelete="CASCADE"), nullable=True)
-    answer_id = Column(Integer, ForeignKey(
+    answer_id = Column(String(50), ForeignKey(
         "answer.answer_id", ondelete="CASCADE"), nullable=False)
     vote_type = Column(String(100), nullable=False)
     owner = relationship('model.User')
@@ -155,19 +154,19 @@ class Like(Base):
     __table_args__ = {'extend_existing': True}
     like_id = Column(Integer, primary_key=True)
     like_type = Column(String(100), nullable=False)
-    user_id = Column(Integer, ForeignKey(
+    user_id = Column(String(50), ForeignKey(
         'user.user_id', ondelete="CASCADE"), primary_key=True)
-    question_id = Column(Integer, ForeignKey(
+    question_id = Column(String(50), ForeignKey(
         'question.question_id', ondelete="CASCADE"), primary_key=True)
 
 
 class Notification(Base):
     __tablename__ = "notification"
     __table_args__ = {'extend_existing': True}
-    notification_id = Column(Integer, primary_key=True, nullable=False)
-    owner_id = Column(Integer, ForeignKey(
+    notification_id = Column(String(50), primary_key=True, nullable=False)
+    owner_id = Column(String(50), ForeignKey(
         "user.user_id", ondelete="CASCADE"), nullable=False)
-    content_id = Column(Integer, ForeignKey(
+    content_id = Column(String(50), ForeignKey(
         "answer.answer_id", ondelete="CASCADE"), nullable=False)
     owner = relationship('model.User')
     # content = relationship('model.Answer')
@@ -179,10 +178,10 @@ class Notification(Base):
 class NotificationTransaction(Base):
     __tablename__ = "transaction_notification"
     __table_args__ = {'extend_existing': True}
-    notification_id = Column(Integer, primary_key=True, nullable=False)
-    owner_id = Column(Integer, ForeignKey(
+    notification_id = Column(String(50), primary_key=True, nullable=False)
+    owner_id = Column(String(50), ForeignKey(
         "user.user_id", ondelete="CASCADE"), nullable=False)
-    content_id = Column(Integer, ForeignKey(
+    content_id = Column(String(50), ForeignKey(
         "transactions.transaction_id", ondelete="CASCADE"), nullable=False)
     owner = relationship('model.User')
     content = relationship('model.Transaction')
@@ -204,7 +203,7 @@ question_tags = Table(
 class Tag(Base):
     __tablename__ = 'tag'
     __table_args__ = {'extend_existing': True}
-    tag_id = Column(Integer, primary_key=True, nullable=False)
+    tag_id = Column(String(50), primary_key=True, nullable=False)
     tag_name = Column(String(40), nullable=False)
     questions = relationship("model.Question",
                              secondary="question_tags", back_populates="tags")
@@ -214,7 +213,7 @@ class Blog(Base):
 
     __tablename__ = 'blog'
     __table_args__ = {'extend_existing': True}
-    blog_id = Column(Integer, primary_key=True, nullable=False)
+    blog_id = Column(String(50), primary_key=True, nullable=False)
     title = Column(String(300), nullable=False)
     body = Column(String(7000), nullable=False)
     author = Column(String(300), nullable=False)
@@ -224,9 +223,9 @@ class Blog(Base):
     user = relationship('model.User')
     date_posted = Column(TIMESTAMP(timezone=True),
                          nullable=False, server_default=text('now()'))
-    blog_user_id = Column(Integer, ForeignKey(
+    blog_user_id = Column(String(50), ForeignKey(
         "user.user_id", ondelete="CASCADE"), nullable=False)
-
+ 
 
 # create tables
 Base.metadata.create_all(bind=engine)
