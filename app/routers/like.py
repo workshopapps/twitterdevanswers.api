233cac34601af2d41547d90e58183fe020c7109c
timmySpark <timmyspark1@gmail.com>
2023-03-16 01:23:51 +0000

    feat :- rewrote add_like logic  , created endpoints for checking total likes in comments and topics endpoints

--- app/routers/like.py
+++ app/routers/like.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from app.routers.answer import get_question
 from app.routers.notification import create_notification
+from uuid import uuid4
 
 
 router = APIRouter(
@@ -14,127 +15,83 @@ router = APIRouter(
 
 
 @router.get("/{question_id}")
-def list_like(question_id, db: Session = Depends(get_db)):
-    """ List all likes for a specific question """
+def list_like_question(question_id, db: Session = Depends(get_db)):
+    """ List all likes for a specific Question """
 
-    return db.query(model.Like).filter(model.Like.question_id == question_id).all()
+    return db.query(model.Like).filter(model.Like.item_id == question_id).all()
 
 
-@router.post("/")
-def create_like(like_base: schema.Like, background_task: BackgroundTasks, db: Session = Depends(get_db),
-                current_user: int = Depends(oauth.get_current_user)):
-    """ Add Like endpoint for a specific question """
 
-    # get question
-    db_question = get_question(db=db, question_id=like_base.question_id)
-    if db_question is None:
-        raise HTTPException(status_code=404, detail="Invalid Question ID")
+@router.get("/posts/{topic_id}")
+def list_like_post(topic_id, db: Session = Depends(get_db),current_user: str = Depends(oauth.get_current_user)):
+    """ List all likes for a specific Post """
 
-    # get like
-    check_if_like_exist = db.query(model.Like).filter(
-        model.Like.question_id == like_base.question_id, model.Like.user_id == current_user.user_id
-    ).first()
+    return db.query(model.Like).filter(model.Like.item_id == topic_id).all()
 
-    # check_if_like_exist
-    if check_if_like_exist is not None:
 
-        if check_if_like_exist.like_type == "up" and like_base.like_type == "up":
-            pass
 
-        elif check_if_like_exist.like_type != "up" and like_base.like_type == "up":
-            # update question
-            question_db = db.query(model.Question).filter(
-                model.Question.question_id == like_base.question_id).first()
-            question_db.total_like = question_db.total_like + 1
-            question_db.total_unlike = question_db.total_unlike - 1
-            # update like
-            check_if_like_exist.like_type = like_base.like_type
+@router.get("/comments/{comment_id}")
+def list_like_comment(comment_id, db: Session = Depends(get_db),current_user: str = Depends(oauth.get_current_user)):
+    """ List all likes for a specific Comment """
 
-            db.commit()
-            db.refresh(question_db)
-            db.refresh(check_if_like_exist)
+    return db.query(model.Like).filter(model.Like.item_id == comment_id).all()
 
-        elif check_if_like_exist.like_type == "up" and like_base.like_type != "up":
-            # update question
-            question_db = db.query(model.Question).filter(
-                model.Question.question_id == like_base.question_id).first()
-            question_db.total_unlike = question_db.total_unlike + 1
-            question_db.total_like = question_db.total_like - 1
-            # update like
-            check_if_like_exist.like_type = like_base.like_type
 
-            db.commit()
-            db.refresh(question_db)
-            db.refresh(check_if_like_exist)
-        else:
-            pass
-
-        return {"detail": "Success"}
+@router.post('/add_like/')
+def add_like(id: str, item_type: str, background_task: BackgroundTasks, db: Session = Depends(get_db), current_user: str = Depends(oauth.get_current_user)):
+    # Check if the specified ID and item_type exist in the database
+    
+    if item_type == 'question':
+        item = db.query(model.Question).filter_by(question_id=id).first()
+    elif item_type == 'topic':
+        item = db.query(model.Topic).filter_by(topic_id=id).first()
+    elif item_type == 'comment':
+        item = db.query(model.Comment).filter_by(comment_id=id).first()
     else:
+        raise HTTPException(status_code=400, detail=f"Invalid item_type: {item_type}")
 
-        db_like = model.Like(
-            user_id=current_user.user_id,
-            like_type=like_base.like_type,
-            question_id=like_base.question_id,
-            like_id=+1
-        )
+    if not item:
+        raise HTTPException(status_code=404, detail=f"{item_type.capitalize()} not found")
 
-        # add like
-        question_db = db.query(model.Question).filter(
-            model.Question.question_id == like_base.question_id).first()
-        if like_base.like_type == "up":
-            question_db.total_like = question_db.total_like + 1
-        else:
-            question_db.total_unlike = question_db.total_unlike + 1
+    # Check if the user has already liked the item
+    if db.query(model.Like).filter_by(user_id=current_user.user_id, item_id=id, item_type=item_type).first():
+        raise HTTPException(status_code=400, detail=f"You have already liked this {item_type}")
 
-        db.add(db_like)
-        db.commit()
-        db.refresh(db_like)
-        db.refresh(question_db)
+    # Add a new like
+    like = model.Like(
+        like_id=uuid4(),
+        item_id=id,
+        user_id=current_user.user_id,
+        item_type=item_type
+    )
+    # Update the item's like count
+    if item_type == 'question':
+        item.total_like += 1
+    elif item_type == 'topic':
+        item.total_likes += 1    
+    elif item_type == 'comment':    
+        item.total_reactions += 1
 
-        # This automatically creates a notification by calling create_notification as a background function which
-        # runs after returning a response
-        notification = schema.NotificationCreate(
-            owner_id=question_db.owner_id,
-            content_id=db_question.question_id,
-            type="Like",
-            title=f"@{current_user.username} liked your question.",
-        )
-        background_task.add_task(
-            create_notification, notification=notification, db=db)
+    db.add(like)
+    db.commit()
 
-        return db_like
+    
+    # This automatically creates a notification by calling create_notification as a background function which
+    # runs after returning a response
+    notification = schema.NotificationCreate(
+        owner_id=like.user_id,
+        content_id=like.item_id,
+        type=like.item_type,
+        title=f"@{current_user.username} liked your {item_type}.",
+    )
+    background_task.add_task(
+        create_notification, notification=notification, db=db)
 
-# @router.post("/", status_code=status.HTTP_201_CREATED)
-# def like(like: schema.Like, db: Session = Depends(get_db), current_user: int = Depends(oauth.get_current_user)):
-#     question = db.query(model.Question).filter(
-#         model.Question.question_id == like.question_id).first()
-#     if not question:
-#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-#                             detail=f"Question with id: {like.question_id} does not exist.")
-#
-#     like_query = db.query(model.Like).filter(
-#         model.Like.question_id == like.question_id,
-#         model.Like.user_id == current_user.user_id
-#     )
-#     found_like = like_query.first()
-#     if (like.dir == 1):
-#         if found_like:
-#             raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-#                                 detail=f"user {current_user.user_id} has already voted on post {like.question_id}")
-#
-#         new_like = model.Like(question_id=like.question_id,
-#                               user_id=current_user.user_id)
-#         db.add(new_like)
-#         db.commit()
-#
-#         return {"message": "successfully added Like to Question"}
-#     else:
-#         if not found_like:
-#             raise HTTPException(
-#                 status_code=status.HTTP_404_NOT_FOUND, detail="like does not exist")
-#
-#         like_query.delete(synchronize_session=False)
-#         db.commit()
-#
-#         return {"message": "successfully removed like"}
+    # Return the new like data as a JSON response
+    return {"success": True, "data": {
+        "like_id": like.like_id,
+        "item_id": like.item_id,
+        "user_id": like.user_id,
+        "item_type": like.item_type
+    }}
+
